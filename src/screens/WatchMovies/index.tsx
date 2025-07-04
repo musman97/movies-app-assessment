e681f32cb5e5193cdd2d2102e2dbb2d82493f143
Muhammad Usman <zahoor.usman.m@gmail.com>
2025-07-04 13:20:54 +0500

    fix: Remove hello text

--- src/screens/WatchMovies/index.tsx
+++ src/screens/WatchMovies/index.tsx
@@ -4,11 +4,5 @@ import {ScreenContainer} from '~/components';
 import {Fonts} from '~/theme';
 
 export function WatchMoviesScreen(): JSX.Element {
-  return (
-    <ScreenContainer>
-      <Text style={{fontFamily: 'Poppins_400Regular', fontSize: 32}}>
-        Hello
-      </Text>
-    </ScreenContainer>
-  );
+  return <ScreenContainer></ScreenContainer>;
 }
