6ec46b3f5254572cd096e8457357cc2d7ca3762d
Muhammad Usman <zahoor.usman.m@gmail.com>
2025-07-04 13:21:23 +0500

    refactor: Remove unwanted imports

--- src/screens/WatchMovies/index.tsx
+++ src/screens/WatchMovies/index.tsx
@@ -1,7 +1,5 @@
 import {JSX} from 'react/jsx-runtime';
-import {Text} from 'react-native';
 import {ScreenContainer} from '~/components';
-import {Fonts} from '~/theme';
 
 export function WatchMoviesScreen(): JSX.Element {
   return <ScreenContainer></ScreenContainer>;
